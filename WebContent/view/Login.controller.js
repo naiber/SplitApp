10bf932eb4c32258287906dec41bf77b19baf784
unknown <naiberxx@gmail.com>
2018-04-16 17:12:48 +0200

    set link to logon

--- WebContent/view/Login.controller.js
+++ WebContent/view/Login.controller.js
@@ -12,30 +12,6 @@ sap.ui.controller("sap.ui.SplitApp.view.Login", {
 		this.getView().setModel(userLogged);
 	},
 	
-//	getStatusToken : function(url){
-//		var res = new sap.ui.model.json.JSONModel();
-//		res.loadData(url,null,true,'GET');
-////		var b = res.attachRequestCompleted(function(oEvent){
-////			var out = false;
-////			if(oEvent.getParameter("message")==="OK"){
-////				out = true;
-////			}
-////			return out;
-////		})
-////		console.log("out-->",b)
-////		return b;
-//		res.attachRequestCompleted(function(oEvent){
-//			console.log("data",res.getData());
-//		})
-////		return res.getProperty("/oData/status")
-////		var data = new sap.ui.model.json.JSONModel();
-////		data = res.attachRequestCompleted(function(){
-////			var out;
-////			out = res.getData();
-////			return out;
-////		})
-////		return data;
-//	},
 	
 	submitPressed : function(oEvent){
 		console.log("dentro submitPressed")
@@ -46,7 +22,7 @@ sap.ui.controller("sap.ui.SplitApp.view.Login", {
 				"username" : user,
 				"password" : password
 		};
-		var url = "http://127.0.0.1:8000/api/db/login";
+		var url = "https://servernodeforsapui5.herokuapp.com/users/logon";
 		
 		sap.ui.SplitApp.Gateway.post(url,dataToPost, function(error, data) {
 			if(error) return error
@@ -65,7 +41,7 @@ sap.ui.controller("sap.ui.SplitApp.view.Login", {
 		eventBus.publish("AppChannel", "toApp", data);
 	},
 
-/**
+	/**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
 * @memberOf zui_plancal.Login
@@ -100,48 +76,5 @@ sap.ui.controller("sap.ui.SplitApp.view.Login", {
 //
 //	}
 
+
 });
-
-
-
-
-//function asincrona(cb) {
-//	$.jax....
-//		success: function(data) {
-//			cb(null, data)
-//		}, error: function(error) {
-//			cb(error, null)
-//		}
-//}
-//
-//
-//
-//function a () {
-//	return 'b';
-//}
-//
-//var c = a();
-//
-//asincrona(function(error, data) {
-//	if (error)
-//		return new Error()
-//	
-//	data
-//})
-//
-//function asyncornaSync(a, b) {
-//	return new Promise((resolve, reject) => {
-//		$.jax....
-//		success: function(data) {
-//			resolve(data)
-//		}, error: function(error) {
-//			reject(error)
-//		}
-//	})
-//}
-//
-//asyncornaSync(a, b).then().catch()
-
-//fuction a(){}
-//var b = a;
-//b()
